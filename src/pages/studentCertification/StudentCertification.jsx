8072b57b6e7051efc766fa7c92fd4600a81dc4ab
DoanPNK <doanpnk@splus-software.com.vn>
2019-11-18 23:42:09 +0700

    add student certification page

---
+++ src/pages/studentCertification/StudentCertification.jsx
@@ -0,0 +1,30 @@
+import React, { PureComponent } from 'react';
+import DataTable from 'shared/components/dataTable/DataTable';
+import Filters from 'shared/components/filters/Filters';
+import ContainedButton from 'shared/components/containedButton/ContainedButton';
+
+export class StudentCertification extends PureComponent {
+
+    render() {
+        return (
+            <div>
+                <div style={{ fontSize: '40px' }}>Xác Nhận Sinh Viên</div>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Filters />
+                    <ContainedButton label='Lọc sinh viên' />
+                </div>
+                <DataTable />
+                <div style={{display:'flex',justifyContent:'row'}}>
+                    <ContainedButton label='Thêm' />
+                    <ContainedButton label='Xem danh sách in' />
+                    <ContainedButton label='In' />
+                    <ContainedButton label='Xuất' />
+
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default StudentCertification;
