4fa40d112dbd9af3b5cbade7515b63108dee15ac
DoanPNK <doanpnk@splus-software.com.vn>
2019-11-21 17:30:01 +0700

    add main dialog add student

--- src/pages/studentCertification/StudentCertification.jsx
+++ src/pages/studentCertification/StudentCertification.jsx
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import DataTable from 'shared/components/dataTable/DataTable';
 import Filters from 'shared/components/filters/Filters';
 import ContainedButton from 'shared/components/containedButton/ContainedButton';
+import BeforeStudentCertificateDialog from 'pages/beforeStudentCertificateDialog/BeforeStudentCertificateDialog';
 
 export class StudentCertification extends PureComponent {
 
@@ -14,8 +15,8 @@ export class StudentCertification extends PureComponent {
                     <ContainedButton label='Lọc sinh viên' />
                 </div>
                 <DataTable />
-                <div style={{display:'flex',justifyContent:'row'}}>
-                    <ContainedButton label='Thêm' />
+                <div style={{ display: 'flex', justifyContent: 'row' }}>
+                    <BeforeStudentCertificateDialog />
                     <ContainedButton label='Xem danh sách in' />
                     <ContainedButton label='In' />
                     <ContainedButton label='Xuất' />
--- src/shared/components/filter/Filter.jsx
+++ src/shared/components/filter/Filter.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
@@ -8,22 +7,20 @@ import Select from '@material-ui/core/Select';
 const useStyles = makeStyles(theme => ({
     formControl: {
         margin: theme.spacing(1),
-        minWidth: 120,
+        minWidth: 'fit-content',
     },
     selectEmpty: {
         marginTop: theme.spacing(2),
     },
+    select: {
+        padding: 10,
+        paddingRight: 35
+    }
 }));
 
 export default function SimpleSelect(props) {
     const classes = useStyles();
     const [selectItem, setSelectItem] = React.useState(0);
-
-    const inputLabel = React.useRef(null);
-    const [labelWidth, setLabelWidth] = React.useState(0);
-    React.useEffect(() => {
-        setLabelWidth(inputLabel.current.offsetWidth);
-    }, []);
 
     const handleChange = event => {
         setSelectItem(event.target.value);
@@ -36,21 +33,20 @@ export default function SimpleSelect(props) {
             );
         });
     };
-
     const { label } = props;
-
     return (
-        <div>
+        <div style={{ display: 'flex', alignContent: 'baseline', height: 'fit-content' }}>
+            {label &&
+                <span style={{ minWidth: 'fit-content', alignSelf: 'center' }}>{label}</span>
+            }
             <FormControl variant="outlined" className={classes.formControl}>
-                <InputLabel ref={inputLabel} id="demo-simple-select-outlined-label">
-                    {label}
-                </InputLabel>
                 <Select
                     labelId="demo-simple-select-outlined-label"
                     id="demo-simple-select-outlined"
                     value={selectItem}
                     onChange={handleChange}
-                    labelWidth={labelWidth}
+                    inputProps={{ className: classes.select }}
+                // style={{ width: 'max-content' }}
                 >
                     {drawData()}
                 </Select>
