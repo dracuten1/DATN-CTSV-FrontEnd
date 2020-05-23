57c3b37081dc8a94418725223a02fec747bc13eb
DoanPNK <yanmad27@gmail.com>
2020-05-23 14:55:46 +0700

    Make build script depend on OS

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -1,8 +1,10 @@
 import prod from "./app-config-prod";
 import dev from "./app-config-dev";
 import local from "./app-config-local";
+import { getOperatingSystem } from 'core/DetectOSAndBrowser/index';
 
-const appStage = process.env.REACT_APP_STAGE.trim();
+
+const appStage = getOperatingSystem(window) === 'Linux OS' ? process.env.NODE_ENV.trim() : process.env.REACT_APP_STAGE.trim();
 
 const isDev =
   appStage !== undefined && appStage !== null && appStage.trim() === "development";
@@ -14,7 +16,7 @@ let config = local;
 
 if (isDev) {
   config = dev;
-  
+
 } else if (isProduction) {
   config = prod;
 }
