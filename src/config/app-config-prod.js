7f67bf4f3d1c8ab00dd4f72ae011b7b3a4f7bba0
DoanPNK <yanmad27@gmail.com>
2020-05-23 22:33:11 +0700

    Create new httpclient for new domain api

--- src/config/app-config-prod.js
+++ src/config/app-config-prod.js
@@ -4,5 +4,6 @@ export default {
   },
   api: {
     BASE_URL: "https://dg3wrybebk.execute-api.ap-southeast-1.amazonaws.com/dev/",
+    BASE_URL2: "https://cj0on7dbo3.execute-api.ap-southeast-1.amazonaws.com/dev/"
   },
 };
